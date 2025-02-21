526aca2fb2b90923eb7aa819345df0791fff6d8a
kristinamsoo <kristinamsoo1@gmail.com>
2025-02-21 16:42:33 +0200

    Update index.js

--- veebiapp/index.js
+++ veebiapp/index.js
@@ -5,7 +5,18 @@ const app = express()
 
 
 app.get('/tervitus', (req, res) => {
-   res.end('Tere maailm')
+   console.log(req.query)
+   res.send(`
+      <htm>
+         <head>
+         </head>
+         <body>
+            <h1>Tervitused.</h1> <br> <p style=color:blue;">Minu koer ${req.query.nimi} on maailma kõige sõbralikum koer.</p>
+         </body>
+      </html>
+
+
+      `)
 })
 
 
